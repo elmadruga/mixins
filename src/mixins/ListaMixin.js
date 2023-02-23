f819d4b0a83609d8e1713f717cadab9dd5d580a5
Junior Alves <momo.ju@hotmail.com>
2023-02-22 21:51:08 -0300

    praticando com mixins

---
+++ src/mixins/ListaMixin.js
@@ -0,0 +1,15 @@
+export default {
+    data: () => ({
+        itens: [],
+        item: ''
+    }),
+    methods: {
+        adicionarItem() {
+            this.itens.push(this.item)
+            this.item = ''
+        }
+    },
+    created() {
+        console.log('ListaMixin criado')
+    }
+}
